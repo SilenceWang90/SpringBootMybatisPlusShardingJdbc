82486a8bfba7c3b7084f368ff337f57d9bdc420f
wangpeng <wangpeng0717@sina.cn>
2024-11-24 09:17:53 +0800

    初始化工程：学习Mybatis-Plus集成多数据源，以及其中一个数据源是shardingJDBC，验证shardingJDBC的各种使用场景

---
+++ src/main/java/com/wp/SpringBootMybatisPlusShardingJdbcApplication.java
@@ -0,0 +1,9 @@
+package com.wp;
+
+/**
+ * @author wangpeng
+ * @description SpringBootMybatisPlusShardingJdbcApplication
+ * @date 2024/11/24 09:13
+ **/
+public class SpringBootMybatisPlusShardingJdbcApplication {
+}
