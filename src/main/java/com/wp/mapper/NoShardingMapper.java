ad33b9674e65333518bdd72c748fa7e8b3663ecd
wangpeng <wangpeng0717@sina.cn>
2024-12-04 16:35:29 +0800

    shardingjdbc配置信息注释补充

--- src/main/java/com/wp/mapper/NoShardingMapper.java
+++ src/main/java/com/wp/mapper/NoShardingMapper.java
@@ -15,6 +15,6 @@ public interface NoShardingMapper {
     @Select("select * from no_sharding where id = #{id}")
     NoSharding selectNoSharding(@Param("id") Integer id);
 
-    @Select("select no_sharding.* from no_sharding inner join test on test.id = no_sharding.id where test.id = #{id}")
+    @Select("select no_sharding.* from no_sharding left join test on test.id = no_sharding.id and test.id = #{id}")
     List<NoSharding> selectWithJoin(@Param("id") Integer id);
 }
