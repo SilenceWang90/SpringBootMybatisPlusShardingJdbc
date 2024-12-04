c4ce57fe2665237df5a2e6105b94a4fe8a713132
wangpeng <wangpeng0717@sina.cn>
2024-12-04 16:07:43 +0800

    shardingjdbc配置信息注释补充

--- src/main/java/com/wp/mapper/NoShardingMapper.java
+++ src/main/java/com/wp/mapper/NoShardingMapper.java
@@ -15,6 +15,8 @@ public interface NoShardingMapper {
     @Select("select * from no_sharding where id = #{id}")
     NoSharding selectNoSharding(@Param("id") Integer id);
 
+    // 以下未生效，但是感觉是配置或者是版本的问题，同样的见过生效的
+//    @Select("select no_sharding.* from no_sharding left join test on test.id = no_sharding.id and test.id = #{id}")
     @Select("select no_sharding.* from no_sharding inner join test on test.id = no_sharding.id where test.id = #{id}")
     List<NoSharding> selectWithJoin(@Param("id") Integer id);
 }
